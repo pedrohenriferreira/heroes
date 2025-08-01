4a8e878996ae02a797d849b689c967121d33b1a2
pedrohenriferreira <asp.pedroferreira@gmail.com>
2025-08-01 13:30:12 -0300

    tentativa na correção de bug no deploy

--- api/superheroProxy.js
+++ api/superheroProxy.js
@@ -1,19 +1,13 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
 const API_KEY = "11f01a579d44c5255bc4fe1ec0fc3792";
 const BASE_URL = `https://superheroapi.com/api.php/${API_KEY}`;
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   try {
     const fullPath = req.url || '';
-    
-    // Remove prefix e inclui query params se houver
     const cleanedPath = fullPath.replace(/^\/api\/superheroProxy/, '');
-
     const apiUrl = `${BASE_URL}${cleanedPath}`;
     const apiResponse = await fetch(apiUrl);
     const data = await apiResponse.json();
-
     res.status(apiResponse.status).json(data);
   } catch (error) {
     console.error('Proxy error:', error);
